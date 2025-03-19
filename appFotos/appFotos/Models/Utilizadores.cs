cc07927c9340e5304877d1c739a93e4603c91fa5
Joao Silva <jotal96@gmail.com>
2025-03-19 09:59:21 +0000

    Adição do Controller de Utilizadores Campo país passa a ser opcional Outros campos já têm mensagens de erro personalizadas

--- appFotos/appFotos/Models/Utilizadores.cs
+++ appFotos/appFotos/Models/Utilizadores.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace appFotos.Models;
@@ -16,51 +17,59 @@ public class Utilizadores
     /// <summary>
     /// nome do utilizador
     /// </summary>
+    [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")] 
     public string Nome { get; set; }
     
     /// <summary>
     /// número de identificação fiscal do utilizador 
     /// </summary>
+    [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")] 
     public string NIF { get; set; }
     
     /// <summary>
     /// número de telemóvel do utilizador
     /// </summary>
+    [Display(Name = "Telemóvel")]
+    [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")] 
     public string Telemovel { get; set; }
     
     /// <summary>
     /// Morada do utilizador
     /// </summary>
+    [Required(ErrorMessage = "A {0} é de preenchimento obrigatório.")] 
     public string Morada { get; set; }
     
     /// <summary>
     /// Código Postal da  morada do utilizador
     /// </summary>
+    [Display(Name = "Código Postal")]
+    [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")] 
     public string CodPostal { get; set; }
     
     /// <summary>
     /// País da morada do utilizador
     /// </summary>
-    public string Pais { get; set; }
+    [Display(Name = "País")]
+    public string? Pais { get; set; }
     
     /* *************************
      * Definção dos relacionamentos
      * **************************
      */
-    
+
     /// <summary>
     /// Lista das fotografias que são propriedade do 
     /// utilizador
     /// </summary>
-    public ICollection<Fotografias> ListaFotos { get; set; }
-    
+    public ICollection<Fotografias> ListaFotos { get; set; } = [];
+
     /// <summary>
     /// Lista dos 'gostos' de fotografias do utilizador
     /// </summary>
-    public ICollection<Gostos> ListaGostos { get; set; }
-    
+    public ICollection<Gostos> ListaGostos { get; set; } = [];
+
     /// <summary>
     /// Lista das fotografias compradas pelo utilizador
     /// </summary>
-    public ICollection<Compras> ListaCompras { get; set; }
+    public ICollection<Compras> ListaCompras { get; set; } = [];
 }
