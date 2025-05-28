6e244ebf5d56d87f1d1bfd629da9172fa2477c09
Joao Silva <jotal96@gmail.com>
2025-05-28 11:07:26 +0100

    adicionados controllers público e privado de Fotografias. Adicionada classe DTO(Date Transfer Object)

---
+++ appFotos/appFotos/Models/ApiModels/FotosAutDto.cs
@@ -0,0 +1,8 @@
+namespace appFotos.Models.ApiModels;
+
+public class FotosAutDto
+{
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public string Ficheiro { get; set; }
+}
