beee984ed69f6f61c9541e01deaf44295e1f037d
Joao Silva <jotal96@gmail.com>
2025-06-04 10:32:24 +0100

    Alterado Hub para permitir que gostos sejam feitos em real-time para todos os utilizadores.
    Apenas users autenticados podem fazer gosto

--- appFotos/appFotos/Services/WebSockets/GostosHub.cs
+++ appFotos/appFotos/Services/WebSockets/GostosHub.cs
@@ -1,6 +1,8 @@
 using appFotos.Data;
+using appFotos.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace appFotos.Services.WebSockets;
 
@@ -23,6 +25,42 @@ public class GostosHub : Hub
         Clients.All.SendAsync("OnConnectedAsync", "Temos uma nova conexão: "+Context.ConnectionId);
         
         return base.OnConnectedAsync();
+    }
+
+    [Authorize]
+    public void AtualizarGostos(int idFoto)
+    {
+        // 1 - verificar se a foto existe
+        var foto= _applicationDbContext.Fotografias.Find(idFoto);
+        if (foto == null)
+            return;
+        
+        // 2 - validar se o utilizador já gostou da foto 
+        var utilizador = _applicationDbContext.Utilizadores
+            .Include(u=> u.ListaGostos)
+            .First(u => u.IdentityUserName == Context.User.Identity.Name);
+
+        var listaGostoFoto = utilizador.ListaGostos
+            .Where(ul => ul.FotografiaFk == idFoto);
+
+        // se entra aqui é porque existe um gosto
+        if (listaGostoFoto.Any())
+        {
+            _applicationDbContext.Gostos.Remove(listaGostoFoto.First());
+        }
+        // se entra aqui é porque não existe um gosto
+        else
+        {
+            var novoGosto = new Gostos(){FotografiaFk = idFoto, Data = DateTime.Now, UtilizadorFk = utilizador.Id};
+            _applicationDbContext.Gostos.Add(novoGosto);
+        }
+    
+        _applicationDbContext.SaveChanges();
+        
+        var numGostos = _applicationDbContext.Gostos.Where(g => g.FotografiaFk==idFoto).Count();
+        
+        Clients.All.SendAsync("AtualizarGostos", idFoto, numGostos);
+
     }
     
 }
